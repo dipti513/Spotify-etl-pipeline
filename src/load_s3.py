d855e3d428c2be862061f32204a4981577382614
dipti513 <dmoni22jul@gmail.com>
2025-12-03 23:49:37 +0530

    Refactor load_s3.py by removing unnecessary comments
    Removed comments and simplified the code structure for clarity.

--- src/load_s3.py
+++ src/load_s3.py
@@ -8,24 +8,19 @@ def upload_to_s3(df):
     Uploads a Pandas DataFrame to AWS S3 as a CSV file.
     """
     try:
-        # 1. Initialize S3 Client
         s3 = boto3.client(
             's3',
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
             region_name=AWS_REGION
         )
-        
-        # 2. Prepare the Buffer (In-memory CSV)
-        # We use StringIO so we don't have to save a file to your hard drive first.
+
         csv_buffer = StringIO()
         df.to_csv(csv_buffer, index=False)
         
-        # 3. Create a unique filename
-        # We add a timestamp so we don't overwrite yesterday's data.
+    
         filename = f"raw_data/spotify_tracks_{datetime.now().strftime('%Y%m%d_%H%M%S')}.csv"
         
-        # 4. Upload
         print(f"☁️ Uploading {len(df)} rows to S3 Bucket: {AWS_BUCKET_NAME}...")
         
         s3.put_object(
@@ -39,8 +34,6 @@ def upload_to_s3(df):
     except Exception as e:
         print(f"❌ Error in upload_to_s3: {e}")
 
-# Simple test block
 if __name__ == "__main__":
-    # If you run this file directly, it will fail because 'df' isn't defined.
-    # We need to run this from run_pipeline.py
+    
     print("Run run_pipeline.py instead.")
